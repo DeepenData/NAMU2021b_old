1f2b20964f2879866f8eaab0699cc95026b27fa2
Manuel M-G <manuel.munoz.g@outlook.com>
2021-01-09 17:49:37 -0300

    Modifica nombres en metabolites2reactions

--- modulo.py
+++ modulo.py
@@ -2,23 +2,24 @@
 # Module: modulo.py
 # Version 0.0.1
 
-def aggregate_metabolites(modelo, *metabolito): # Toma metabolitos
+
+def metabolites2reactions(modelo, metabolitos): # Toma metabolitos
   """Crea un agregado de las reacciones segun un listado de metabolitos. 
 
   Parameters
   ----------
-  modelo:
+  modelo: model
       El modelo inicial del cual se toman las reacciones
-  metabolito : str
-      ID de un metabolito. La función acepta multiples para crear un joint. 
+  metabolitos : list
+      IDs de un metabolito. La función acepta multiples para crear un joint. 
 
   Returns
   -------
-  tupla
-      crea una tupla agregada de ids, nombre de reacciones, y reacciones en si
+  df
+      crea un dataframe pandas agregada de ids, nombre de reacciones, y reacciones en si
   """
-  frozen = modelo.metabolites.get_by_id(metabolito[0]).reactions # Crea el inicio
-  for met in metabolito: # Añade metabolistos al listado de reacciones
+  frozen = modelo.metabolites.get_by_id(metabolitos[0]).reactions # Crea el inicio
+  for met in metabolitos: # Añade metabolistos al listado de reacciones
     frozen = frozen.union(modelo.metabolites.get_by_id(met).reactions)
   
   ids   = [list(frozen)[i].id       for i in range(0,len(frozen))] # List Comprehesion
@@ -27,8 +28,7 @@ def aggregate_metabolites(modelo, *metabolito): # Toma metabolitos
 
   list_of_tuples = list(zip(ids, names, rxns))     # DataFrame en Python; crea triplets
   columns        = ["ids","names","rxns"]   # Titulos de columnas de la tripleta
-  tupla          =  pd.DataFrame(list_of_tuples, columns = columns) # Titulos asignados a la lista de tuplas
+  df             =  pd.DataFrame(list_of_tuples, columns = columns) # Titulos asignados a la lista de tuplas
   
-  return tupla
-
+  return df
 # 
