a7f5a3bf7b551707d2dd418229bdfd7fb2a07b70
Alejandro Acevedo A <a.acevedo.aracena@gmail.com>
2021-01-15 12:37:07 -0300

    Cambios de funciones y cosas

--- Python/toy-fba.py
+++ Python/toy-fba.py
@@ -34,148 +34,112 @@ model.reactions.get_by_id("r1").bounds = (-1.33, 0.0)
 """
 # %% --- Inicialización del modelo
 from cobra import Model, Reaction, Metabolite
-model = Model('toy_fba')
+#del model, r1, r2, r3, r4, r5, r6, r7, r8, solution
 
-# %% --- Creación de metabolitos
+model = Model('toy_fba')
+#
+# --- Creación de metabolitos
 m1 = Metabolite("m1")
 m2 = Metabolite("m2")
 m3 = Metabolite("m3")
 m4 = Metabolite("m4")
 m5 = Metabolite("m5")
 
-""" Best practise: SBML (Systems Biology Markup Language) compliant IDs
-reaction = Reaction('3OAS140')
-reaction.name = '3 oxoacyl acyl carrier protein synthase n C140 '
-reaction.subsystem = 'Cell Envelope Biosynthesis'
-reaction.lower_bound = 0.    # This is the default
-reaction.upper_bound = 1000. # This is the default
-reaction.add_metabolites({ # Reacciones estequiometricas
-    malACP_c: -1.0,
-    h_c: -1.0
-})"""
-# %% --- Creación de reacciones 
+#  --- Creación de reacciones 
 r1 = Reaction("r1")
-r1.add_metabolites({m1 : 1.0})
-
+r1.add_metabolites({m1 : 2.0})
 r2 = Reaction("r2")
 r2.add_metabolites({m1: -1.0, m2: 1.0})
-
 r3 = Reaction("r3")
 r3.add_metabolites({m2 : 1.0})
-
 r4 = Reaction("r4")
 r4.add_metabolites({m2 : -1.0})
-
 r5 = Reaction("r5")
 r5.add_metabolites({m2: -1.0, m3: 1.0})
-
 r6 = Reaction("r6")
-r6.add_metabolites({m1: -1.0, m5: -1.0, m3: 1.0, m4: 1.0})
-
+r6.add_metabolites({m1: -1.0, m5: -1.0, m3: 2.0, m4: 1.0})
 r7 = Reaction("r7")
 r7.add_metabolites({m3: -1.0, m4: -1.0, m1: 1.0, m5:1.0})
-
 r8= Reaction("r8")
-r8.add_metabolites({m3: -1.0, m5: -1.0, m4:1.0})
-
-# %% --- Chequear reacciones 
-import numpy as np
-
-print(
-r3.reaction,
-r4.reaction,
-)
-# %% 
-from cobra.util import create_stoichiometric_matrix
-
-model.add_reactions([r1, r2, r3, r4, r5, r6, r7, r8])
-model.reactions.get_by_id('r3')
-
-# %% --- Crea la reacción para objetivo de optimización
-
-obj = Reaction("obj")
-obj.add_metabolites({m2: -1.0, m3: -1.0, m5: -1.0, m4: 1.0}) 
-
-model.add_reactions([obj])
-model.reactions.get_by_id("obj")
-
-# %% 
-
-#Optimización del modelo
- 
+r8.add_metabolites({m3: -1, m5: -1, m4: 2.0})
+obj = Reaction("obj") # Función objetivo
+obj.add_metabolites({m2: -1.5, m3: -1.5, m5: -2.5, m4: 1.0}) 
+model.add_reactions([r1, r2, r3, r4, r5, r6, r7, r8, obj])
 model.objective = "obj"
-model.reactions.get_by_id("obj").bounds = (0, 1000)
 
-# %% MM  --- 2021-01-15 10:27 --- Exporta un grafo 
+## Optimización del modelo
+solution = model.optimize()
+solution = solution.to_frame()
+all_reactions = [model.reactions[i].reaction  for i in range(0,len(model.reactions)) ]
+solution["all_reactions"] = all_reactions
+
+solution
+### === TERMINA EL TOY ===
+# %% MM --- 2021-01-15 12:11 --- Exporta un grafo
 def cobra2networkx(modelo):
     """Toma un modelo de cobra y genera un grafo bipartito de NetworkX
 
-
-    """    
-    return grafo
-
-import networkx as nx
-from networkx.algorithms.bipartite.matrix import biadjacency_matrix      # Extrae matriz de adyacencia
-from networkx.algorithms.bipartite.matrix import from_biadjacency_matrix # Crea el grafo
-from sklearn.preprocessing import binarize
-from scipy.sparse import csr_matrix
-
-tmp = binarize(create_stoichiometric_matrix(model))
-tmp = csr_matrix(tmp)
-tmp = from_biadjacency_matrix(tmp) 
-
-total_nodes = range(0,len(tmp.nodes(data=False))) # Rango de nodos
-particiones = [tmp.nodes(data=True)[i]["bipartite"] for i in total_nodes] # Tipo nodo
-
-metabolites_nodes = [n for n, d in tmp.nodes(data=True) if d["bipartite"] == 0] # Crea una lista de metabolitos
-metabolites_names = ["m1","m2","m3","m4","m5"] # Añade nombres
-reactions_nodes   = [n for n, d in tmp.nodes(data=True) if d["bipartite"] == 1] # Crea una lista de reacciones
-reaction_names    = ["r1","r2","r3","r4","r5","r6","r7","r8"] # Añade nombres
-
-names_mapped =  dict(zip( metabolites_nodes + reactions_nodes, metabolites_names + reaction_names))
-tmp = nx.relabel_nodes(tmp, names_mapped)
-
-# %% MM --- 2021-01-15 10:28 --- Funcción que aplica atributos desde dos vectores
-def met_atributos(grafo, name, vector, skip = 0):
-    """Toma una lista de atributos y se los asigna a un objeto para Gephi
-
-    Parameters
-    ----------
-    grafo : bipartite_graph
-        Un grafo bipartito de NetworkX 
-    nombre : str
-        Nombre del atributo siendo asignado. Ie. nombre de la columna en Gephi
-    vector : list
-        Una lista con valores para atributos. Se asigna desde el nodo [0] + skip
-    skip : int
-        Cantidad de nodos a omitir. Por defecto, los nodos de los metabolitos
-        van primero en la estructura de datos. Reacciones=len(model.metabolites)
+    Params
+    ------
+    modelo : cobra_model
+        Un modelo de reacciones metabolicas en COBRA
+    
     Returns
     -------
-    grafo : bipartite_graph
-        Un grafo bipartito de NetworkX 
+    grafo : 
+        un grafo bipartito de NetworkX. Incluye atributos de flujo del modelo y
+        sensibilidad de metabolitos y reacciones. 
+    
+    Notes
+    -----
+        - nx.write_gexf(grafo, "grafo.gexf") Crea una salida para Gephi
     """
-    grafo.nodes(data=False) # Consigue nombre de los nodos
-    nodos = metabolites_names + reaction_names # Lista con nombres de nodos
+    import networkx as nx
+    from cobra.util import create_stoichiometric_matrix
 
-    tmp_prices =   solution.shadow_prices.tolist() + ['']*8 # Shadow Prices
-    tmp_fluxes = ['']*5 + solution.fluxes.tolist()          # Flujos
+    from networkx.algorithms.bipartite.matrix import biadjacency_matrix      # Extrae matriz de adyacencia
+    from networkx.algorithms.bipartite.matrix import from_biadjacency_matrix # Crea el grafo
+    from sklearn.preprocessing import binarize
+    from scipy.sparse import csr_matrix
 
+    tmp = binarize(create_stoichiometric_matrix(model)) # Crea una matriz
+    tmp = csr_matrix(tmp) # Convierte la matriz a una matriz dispersa
+    tmp = from_biadjacency_matrix(tmp) # Usa la dispersa para un grafo bipartito
+
+    total_nodes = range(0 , len(tmp.nodes(data=False))) # Rango de nodos
+    particiones = [tmp.nodes(data=True)[i]["bipartite"] for i in total_nodes] # Tipo nodo
+
+    metabolites_nodes = [n for n, d in tmp.nodes(data=True) if d["bipartite"] == 0] # Crea una lista de metabolitos
+    metabolites_n     = len(model.metabolites) # Numero de metabolitos
+    metabolites_names = [model.metabolites[i].id for i in range(0, metabolites_n) ]
+
+    reactions_nodes   = [n for n, d in tmp.nodes(data=True) if d["bipartite"] == 1] # Crea una lista de reacciones
+    reactions_n       = len(model.reactions)   # Numero de reacciones
+    reactions_names   = [model.reactions[i].id   for i in range(0, reactions_n)   ]
+
+    names_mapped =  dict(zip( metabolites_nodes + reactions_nodes, metabolites_names + reaction_names))
+    tmp = nx.relabel_nodes(tmp, names_mapped)
+    # En este punto el grafo ya cuenta con los nodos y labels segun IDs
+
+    solution = model.optimize()    # Esto posiblemente no deberia estar aqui
+    solution = solution.to_frame() # Ni esto tampoco
+
+    # Crea diccionarios con atributos para el grafo
+    
+    # Diccionario para precios (sensibilidad del metabolito) y nx.asignación
+    tmp_prices = solution.shadow_prices.tolist() + (['']*reactions_n) # Shadow Prices
     tmp_prices = { nodos[i] : (tmp_prices)[i] for i in range(0, len(tmp_prices)) } 
-    tmp_fluxes = { nodos[i] : (tmp_fluxes)[i] for i in range(0, len(tmp_prices)) } 
-
     nx.set_node_attributes(tmp, tmp_prices, 'prices' ) # Añade una columna de precios
+    
+    # Diccionario para flujos (flujo de la reaccion) y nx.asignación
+    tmp_fluxes = ['']*metabolites_n + solution.fluxes.tolist()  # Flujos de reacciones
+    tmp_fluxes = { nodos[i] : (tmp_fluxes)[i] for i in range(0, len(tmp_prices)) } 
     nx.set_node_attributes(tmp, tmp_fluxes, 'fluxes' ) # Añade una columna de flujos
-    # ----- END
-    # tmp.nodes(data=True) # VIS: muestra como deberia verse el grafo
-    nx.write_gexf(tmp, "grafo.gexf") # Salida para Gephi
 
     return grafo
 
-# TODO: Convertir esto en una función de verdad f(modelo, filename)
-
-# %% MM --- 2021-01-15 10:24 --- Función toma dos listas y añade atributos
-def list2attr(grafo, nodos, atributos, nombre):
+# %% MM --- 2021-01-15 12:10 --- Función toma dos listas y añade atributos
+def list2attr(grafo, nod_os, atributos, nombre):
     """Toma dos listas de nombres de nodos y atributos y las añade a un grafo
 
     Parameters
@@ -199,15 +163,34 @@ def list2attr(grafo, nodos, atributos, nombre):
     nx.set_node_attributes(grafo, tmp_list, nombre ) # Añade los atributos
     return grafo
 
-# %% --- Algo más que no se que hace ---
+# %% AA 15_ene_2020 --- Stoichiometric matrix to directed bipartite graph
+from sklearn.preprocessing import binarize
+from scipy.sparse import csr_matrix
+import networkx as nx
+from networkx.algorithms.bipartite.matrix import biadjacency_matrix      # Extrae matriz de adyacencia
+from networkx.algorithms.bipartite.matrix import from_biadjacency_matrix # Crea el grafo
+from cobra.util import create_stoichiometric_matrix
 
-solution = model.optimize()
-solution = solution.to_frame()
+stoichiometric_matrix        =  create_stoichiometric_matrix(model)
+stoichiometric_matrix        =  np.where(stoichiometric_matrix < 0, -1, stoichiometric_matrix)
+stoichiometric_matrix        =  np.where(stoichiometric_matrix > 0,  1, stoichiometric_matrix)
 
-all_reactions = [model.reactions[i].reaction  for i in range(0,len(model.reactions)) ]
-solution["all_reactions"] = all_reactions
 
-# Función que tome los flux y los pegue como atributo NetworkX
+sparse_stoichiometric_matrix = csr_matrix(stoichiometric_matrix)
+directed_bipartite_graph     = from_biadjacency_matrix(sparse_stoichiometric_matrix, create_using= nx.DiGraph) 
+
+# row_order (list of nodes) – The rows of the matrix are ordered according to the list of nodes.
+
+incidence_matrix = biadjacency_matrix(directed_bipartite_graph, row_order = range(0,stoichiometric_matrix.shape[0]))
+
+print(
+(incidence_matrix == stoichiometric_matrix).all(),
+directed_bipartite_graph.is_directed())
+
+nx.in_degree_centrality(directed_bipartite_graph)
+nx.out_degree_centrality(directed_bipartite_graph)
+nx.pagerank(directed_bipartite_graph)
+ 
 
 
 # %% --- CENTRALITY
