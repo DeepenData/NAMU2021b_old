9c2bc459431b654f5ad01064b15c290799e1aa6e
Manuel M-G <manuel.munoz.g@outlook.com>
2021-02-06 14:13:00 -0300

    Funcionaliza toy_metabolism_centrality.py

--- Python/toy_metabolism_centrality_AA.py
+++ Python/toy_metabolism_centrality_AA.py
@@ -20,27 +20,23 @@ solution_fba.fluxes # Check si los flujos funcionan
 2. Binarización de matriz de incidencia para eliminar los pesos y dirección de 
    las reacciones. Los coeficientes son un artefacto, cuando solo nos interesa
    la conectividad
-4. Convierte la matriz en una sparce matix, para optimizar el uso de memoria en
-   la computación de redes más grandes
 5. Proyección de la matriz de incidencia hacia el espacio de las reacciones, lo
    que genera en matriz de adyacencia de reacciones
 6. Ceros a la diagonal de la matriz, para eliminar auto-conexiones
 7. Binarización, de nuevo. Solo nos interesa saber si una reaccion comparte 
-   pathway con otra, no cuantos metabolitos comparten"""
+   pathway con otra, no cuantos metabolitos comparten
+4. Convierte la matriz en una sparce matix, para optimizar el uso de memoria en
+   la computación de redes más grandes"""
 
 import numpy as np
 import networkx as nx
 
-from   cobra.util.array import create_stoichiometric_matrix
+from cobra.util.array import create_stoichiometric_matrix
 from sklearn.preprocessing import binarize
-from scipy.sparse import csr_matrix
 
 S_matrix = create_stoichiometric_matrix(model) # Crea matriz de incidencia
-S_matrix = binarize( abs(S_matrix) ) # Binarización de la matriz de incidencia 
- # para eliminar pesos y dirección que no son necesarios en este resultado
-S_matrix = csr_matrix( S_matrix ) # Convierte a matriz dispersa optimizando RAM
-
-# S_matrix = (S_matrix !=0).astype(int) # Binarización de la matriz de incidencia 
+S_matrix = binarize(abs(S_matrix) ) # Binarización de la matriz de incidencia 
+    # para eliminar pesos y dirección que no son necesarios en este resultado
 
 projected_S_matrix = np.matmul(S_matrix.T, S_matrix) # Proyección al espacio de 
   # las reacciones de la matriz en matriz de incidencia
@@ -54,10 +50,13 @@ np.fill_diagonal(projected_S_matrix, 0) # Elimina la conexion consigo mismo
 
 reaction_adjacency_matrix = (projected_S_matrix !=0).astype(int) # Re-binariza, 
     # para eliminar el peso al compartir más de un metabolito entre reacciones
-reaction_adjacency_matrix.shape
 
 # %% --- Crea el grafo a partir de la matriz de adyacencia
-G = nx.from_numpy_matrix(reaction_adjacency_matrix)
+from scipy.sparse import csr_matrix
+
+from networkx.convert_matrix import from_scipy_sparse_matrix
+reaction_adjacency_matrix = csr_matrix(reaction_adjacency_matrix)
+G = from_scipy_sparse_matrix(reaction_adjacency_matrix)
 
 if not (nx.is_connected(G)):
     # Check si este grafo tiene nodos huerfanos
@@ -65,31 +64,143 @@ if not (nx.is_connected(G)):
     largest_component = max(nx.connected_components(G), key=len)
     print ('Removed',len(G.nodes) - len(largest_component), 'nodes.')
     G = G.subgraph(largest_component)
-    
-G.nodes
 
 # %% --- Reasigna nombres a nodos
 
-from networkx import set_node_attributes
-set_node_attributes(G, [reaction.id for reaction in model.reactions], 'name' )
+from networkx import relabel_nodes
 
-# %% --- Definición de subsistemas de interes
+# Crea un diccionario con una lista para cada nodo, en orden
+node_dict = lambda l : dict( zip( list(G.nodes), l ) )
 
-subsystems_1 = []
-subsystems_2 = []
+cursed_dict = node_dict( [reaction.id for reaction in model.reactions] )
+G = relabel_nodes(G, cursed_dict, copy=True)
 
 # %% --- Calculos de centralidades varios
 """Calculos de centralidades baseline,  
 Existen 30+ centralidades, las cuales no aplican en todas las condiciones"""
 
-graph = G.copy()
+def eight_centralities(grafo):
+    """Calcula centralidades base de una red
 
-hc  = nx.harmonic_centrality(graph, nbunch=None, distance=None)
-ec  = nx.eigenvector_centrality(graph, max_iter=1000, tol=1e-05, nstart=None, weight=None)
-dc  = nx.degree_centrality(graph)
-bc  = nx.betweenness_centrality(graph, normalized=True, weight=None, endpoints=False, seed=None)
-"""↓ Here be dragons ↓"""
-cc  = nx.closeness_centrality(graph, distance=None, wf_improved=True)
-lc  = nx.load_centrality(graph, cutoff=None, normalized=True, weight=None)
-ic  = nx.information_centrality(graph)
-soc = nx.second_order_centrality(graph)
+    ```   
+    ['harmonic_centrality', 'eigenvector_centrality', 'degree_centrality', 
+    'betweenness_centrality', 'closeness_centrality', 'load_centrality', 
+    'information_centrality', 'second_order_centrality']
+    ```
+
+    Returns
+    -------
+    centralities : list
+        Una lista de diccionarios {nodo : centralidad} con las centralidades
+        segun la lista descrita arriba."""
+
+    import networkx as nx
+
+    hc  = nx.harmonic_centrality(grafo, nbunch=None, distance=None)
+    ec  = nx.eigenvector_centrality(grafo, max_iter=1000, tol=1e-05, nstart=None, weight=None)
+    dc  = nx.degree_centrality(grafo)
+    bc  = nx.betweenness_centrality(grafo, normalized=True, weight=None, endpoints=False, seed=None)
+
+    """↓ Here be dragons ↓"""
+    cc  = nx.closeness_centrality(grafo, distance=None, wf_improved=True)
+    lc  = nx.load_centrality(grafo, cutoff=None, normalized=True, weight=None)
+    ic  = nx.information_centrality(grafo)
+    soc = nx.second_order_centrality(grafo)
+
+    centralities = [hc, ec, dc, bc, cc, lc, ic, soc]
+
+    return centralities
+
+def assign_eight_centralities(grafo, centralities, prefix='',subfix=''):
+    """Asigna una lista de centralidades creada por eight_centralities() como 
+    atributos
+
+    Parameters
+    ----------
+    grafo : NetworkX graph
+    centralities : list
+        Una lista de centralidades creada por eight_centralities()
+        Puede ser directamente eight_centralities(grafo)
+    prefix : str, default = ''
+        Un prefijo para el atributo de centralidad asignada
+    subfix : str, default = ''
+        Un subfijo para el atributo de centralidad asignada
+
+    Returns
+    -------
+    grafo : NetworkX graph
+    """
+    centralities_names = ['harmonic_centrality', 'eigenvector_centrality', 'degree_centrality', 'betweenness_centrality',
+    'closeness_centrality', 'load_centrality', 'information_centrality', 'second_order_centrality']
+   
+    centralities_names = [ (prefix + name + subfix) for name in cent_list]
+   
+    for i in range(0, 7):
+        nx.set_node_attributes(grafo, centralities[i], centralities_names[i])
+    
+    return graph
+
+centralities = eight_centralities(G) # Calcula las centralidades
+G = assign_eight_centralities(G, centralities) # Asigna centralidades a nodos
+
+# %% --- Calcula centralidades para cada nodo del grafo
+
+def delta_centralities(grafo, list_remove, prefix='',subfix=''):
+	"""Toma un grafo y una lista de nodos y calcula la centralidad del grafo al
+    remover cada uno de esos nodos.
+
+    Prefix y subfix son información sobre la lista de los nodos removidos. El
+    atributo final en el grafo es ≪`PREFIX` REMOVED_nodo[i] `SUBFIX` ≫
+
+    Parameters
+    ----------
+    grafo : 
+        Un grafo de NetworkX 
+    list_remove :
+        Una lista de nodos a remover. Puede ser grafo.nodes
+    prefix : str, default = ''
+    subfix : str, default = ''
+
+    Return
+    ------
+    grafo :
+        Un grafo de NetworkX con atributos de centralidad por cada nodo removido
+	"""
+    for node in list_remove:
+		delta = grafo.copy 
+        removed = prefix + '_REMOVED_' + str(node) + subfix
+		delta.remove_node( node ) # Elimina el nodo de la iteración
+		centralities = eight_centralities(delta)   
+		# Asigna centralidades calculadas a los nodos
+		grafo = assign_eight_centralities(grafo, centralities, subfix=removed)
+   
+    return grafo
+
+# Calcula centralidades delta para cada nodo del grafo
+G = delta_centralities(G, G.nodes) 
+
+# %% --- Celtralidades delta por subsistema
+"""
+subsystem_s =  [r1, r2, r3]
+
+nodes_without_subsystem_s = set(nodes) - set(subsystem_s)
+
+
+
+
+baseline centralities: (C_r1), (C_r2), (C_r3)
+
+
+perturbed centralities: C_r1_node_i_removed, C_r2_node_i_removed, C_r3_node_i_removed
+
+
+
+
+unperturbed: mean(C_r1, C_r2, C_r3)
+perturbed:   mean(C_r1_node_i_removed, C_r2_node_i_removed, C_r3_node_i_removed )
+
+
+node_i_contribution_to_S = np.log2(unperturbed/perturbed)
+
+
+"""
