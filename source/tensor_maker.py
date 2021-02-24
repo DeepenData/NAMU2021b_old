dc781383bcb72fca1afbcdafc1e39ad37d4ff427
Manuel M-G <manuel.munoz.g@outlook.com>
2021-02-23 23:51:03 -0300

    Modifica tensor_maker.py
    Crea un diccionario subsystems con
    - key = subsistema
    - value = lista de nodos en dicho subsistema
    
    Streamline del proceso, funcionaliza creación del tensor por subsistemas
    
    Crea un archivo de salida subsystems_centrality con el tensor final
    
    Crea la función fx() (cambiar nombre) que crea un dataframe pandas a
    partir de un nombre de subsistema que este en el diccionario subsystems
    
    To Do:
    - Cambiar el nombre a fx()
    - Hacer una entrada de subsistemas desde fuera o desde un archivo

--- source/tensor_maker.py
+++ source/tensor_maker.py
@@ -6,53 +6,27 @@
 # %% --- Importando cosas iniciales
 import pickle # Importa los elementos generados por delta_centrality.py
 
-outfile1 = open('./tmp/baseline_centralities','rb'); baseline_centralities = pickle.load(outfile1); outfile1.close()   # Tensor n x m x c
-outfile2 = open('./tmp/perturbed_centralities','rb'); perturbed_centralities = pickle.load(outfile2); outfile2.close() # Tensor 1 x m x c
-
-outfile3 = open('./tmp/index_nodes','rb'); index_nodes = pickle.load(outfile3); outfile3.close() # Indice de largo m == n (todos los nodos)
-# Index de las centralidades calculadas
-index_centralities = ['harmonic_centrality', 'eigenvector_centrality', 'degree_centrality', 'betweenness_centrality', 
-      'closeness_centrality', 'load_centrality', 'information_centrality', 'second_order_centrality']
+infile1 = open('./tmp/baseline_centralities','rb'); baseline_centralities = pickle.load(infile1); infile1.close()   # Tensor n x m x c
+infile2 = open('./tmp/perturbed_centralities','rb'); perturbed_centralities = pickle.load(infile2); infile2.close() # Tensor 1 x m x c
 
 # index_nodes : es la lista de las reacciones
-outfile4 = open('./tmp/breaks','rb'); breaks = pickle.load(outfile4); outfile4.close() # Lista de exceptciones <n
+infile3 = open('./tmp/index_nodes','rb'); index_nodes = pickle.load(infile3); infile3.close() # Indice de largo m == n (todos los nodos)
+# index_centralities : nombres de las centralidades calculadas
+index_centralities = ['harmonic_centrality', 'eigenvector_centrality', 'degree_centrality', 'betweenness_centrality', 
+      'closeness_centrality', 'load_centrality', 'information_centrality', 'second_order_centrality']
 
 import numpy as np
 import pandas as pd
 
-def rcn(tensor): 
-    """Convierte un tensor de dimensiones `(n, m, c)` a uno `(n, c, m)`."""
-    import numpy as np
-    return  np.transpose( tensor, (0,2,1) )
+# S = subsystem [1:s]. S.nodes = subset( N )
 
 # R = Nodo removido [1:r]
 # N = Todos los nodos del modelo [1:n]. len(R) == len(N)
 # C = centrality [1:c]. Son 8 en total
 
-# S = subsystem [1:s]. S.nodes = subset( N )
-
 # %% --- Cosas de tensores
 
-# A_n_c: Centralidades (c = 1:8) de todos los nodos (m=1:1000). Dims = 1000 x 8. // Centralidades base
-A_n_c = baseline_centralities[0] # (m,c) 
-
-# A_1_n_c: Centralidades base como tensor de alto 1. Dims = 1 x 1000 x 8
-A_1_n_c = baseline_centralities # (1,m,c)
-
-# B_r_n_c: Centralidades (c) de los nodos  (n=1, ... NA(en la posición de nodo r) ...,1000) removiendo el nodo r. 
-B_r_n_c = perturbed_centralities # Dims: (r, n, c) ~ 1000 x 999+(NaN) x 8
-
-# C_r_n_c se omite porque parece que C es de centralidades y es confuso y caotico 
-
-# D_r_c_n: Es lo mismo que B_r_n_c pero con las centralidades en la segunda dimensión.
-D_r_c_n = rcn( B_r_n_c )
-
-get_n( [] )
-get_c( [] )
-get_n( [] )
-
-# %%
-def subsystem_tensor( subsystem_nodes, tensor=perturbed_centralities, complete_index=index_nodes, axis_to_index=1):
+def subsystem_tensor( subsystem_nodes, tensor, complete_index=index_nodes, axis_to_index=1):
     """Genera un tensor con un subset de nodos basados en el subsistema pedido
 
     Parameters
@@ -60,8 +34,8 @@ def subsystem_tensor( subsystem_nodes, tensor=perturbed_centralities, complete_i
     subsystem_nodes : list
         Lista de nodos por nombre para el subsistema. 
         Eg. `['OxPhox','GluRxn1']`
-    tensor : numpy.ndarray, default perturbed_centralities
-        Un tensor de dimensiones `(r,n,c)`, by default perturbed_centralities
+    tensor : numpy.ndarray
+        Un tensor de dimensiones `(r,n,c)`, con r > 0
     complete_index : list, default index_nodes
         La lista con el indice por nombre de los nodos, tiene que ser del mismo
         largo que la dimensión `n` del tensor 
@@ -84,57 +58,56 @@ def subsystem_tensor( subsystem_nodes, tensor=perturbed_centralities, complete_i
 
     return tensor[:, subsystem_index ,:]
 
+# %% --- Tensor final en si
+def contrib_to_centrality( ns ): 
+
+    ns_baselines = subsystem_tensor( ns, baseline_centralities )[0] # Set aplanado del baseline. Dims = (ns,c)
+    ns_perturbed = subsystem_tensor( ns, perturbed_centralities )   # Tensor de perturbated. Dims = (r,ns,c)
+
+    ns_mean_baselines = np.mean( ns_baselines.T, axis=1) # Dims = (c,1)
+    ns_mean_perturbed = np.mean( ns_perturbed.T, axis=1) # Dims = (c,r)
+
+    ns_perturbed_square = np.apply_along_axis( np.diag, 0, ns_mean_perturbed) # Tensor con centralidades diagonales. Dims = (c,c,r)
+    ns_perturbed_square_inv = np.linalg.inv( ns_perturbed_square.T ).T # Calcula el inverso de las r matrices (c,c). Dims = (c,c,r)
+
+    baselintes_matmul = lambda square_inv : np.matmul( square_inv, ns_mean_baselines ) # Un helper para iterativos
+    ns_ratios = np.apply_along_axis( baselintes_matmul, 0, ns_perturbed_square_inv) # Ratios en primera fase
+    ns_ratios = np.log2( ns_ratios ) # Fold change de baseline/perturbed. Ie, controbución a centralidad. Dims = (c,r)
+
+    return ns_ratios
+
+# %% --- Ejemplos de subsistemas
+
 import random   # DEMOSTRACIÓN DE SUBSISTEMAS RANDOM
 random.seed(23) # DE DISTINTO LAGRGO Y SIMILAR
 
-ns1 = [ index_nodes[n] for n in random.sample(range(0, 1055), 7)  ]
-ns1 = [ index_nodes[n] for n in random.sample(range(0, 1055), 11) ]
-
-# A_ns_c: Lo mismo que A_n_c pero indexado para ns.
-A_ns1_c = subsystem_tensor( ns1, A_1_n_c )[0]
-A_ns2_c = subsystem_tensor( ns2, A_1_n_c )[0]
-
-# B_r_ns_c: Lo mismo que B_r_n_c pero indexado para ns. // B_n_c[ns, *, *]
-B_r_ns1_c = subsystem_tensor( ns1, B_r_n_c )
-B_r_ns2_c = subsystem_tensor( ns2, B_r_n_c )
-
-# %%
-# E_c: Promedio por centralidad de todos los nodes en ms. 
-E_s1_c = np.mean( A_ns1_c.T, axis=1, keepdims=True ) # .shape = (c,1)
-E_s2_c = np.mean( A_ns2_c.T, axis=1, keepdims=True ) # .shape = (c,1)
-
-#F_c_n:Promedio por centralidad de todos los nodes en ms removiendo n. // E_c.mean(axis='n')
-F_s1_c_n = np.mean( B_r_ns1_c , axis = 1).T #, keepdims=True )
-F_s2_c_n = np.mean( B_r_ns2_c , axis = 1).T #, keepdims=True )
-# Las dimensiones correctas (c,n)
-
-F_s1_c_c_n = np.apply_along_axis( np.diag, 0, F_s1_c_n) # Tensor con centralidades diagonales x nodos (c,c,n)
-F_s1_inv_c_c_n = np.linalg.inv(F_s1_c_c_n.T).T # Calcula el inverso de las n matrices (c,c).  shape = (c,c,n)
-
-F_s2_c_c_n = np.apply_along_axis( np.diag, 0, F_s2_c_n) # Tensor con centralidades diagonales x nodos (c,c,n)
-F_s2_inv_c_c_n = np.linalg.inv(F_s2_c_c_n.T).T # Calcula el inverso de las n matrices (c,c).  shape = (c,c,n)
-
-#F_inv_c_c_n.shape
-
-capa_i_s1 = lambda capa_i : np.matmul( capa_i , E_s1_c ) 
-capa_i_s2 = lambda capa_i : np.matmul( capa_i , E_s2_c ) 
-
-Ratios_s1 = np.apply_along_axis( capa_i, 0, F_s1_inv_c_c_n)
-Ratios_s2 = np.apply_along_axis( capa_i, 0, F_s2_inv_c_c_n)
-
-FC_s1  = np.log2(Ratios_s1[0].T)
-FC_s2  = np.log2(Ratios_s2[0].T)
-
-FC_final = np.asarray( [FC_s1, FC_s2] )      # Shape (s,n,c)
-FC_final = np.transpose( FC_final, (2,1,0) ) # Shape (n,c,s)
-
+# EJEMPLO DUMMY DE SUBSISTEMAS
 subsystems = {
-    'name1' : [reaction1, reaction2, reaction3], 
-    'name2' : [reaction3, reaction5, reaction9]
+    'ns1' : [ index_nodes[n] for n in random.sample(range(0, 1055), 7)  ] ,
+    'ns2' : [ index_nodes[n] for n in random.sample(range(0, 1055), 11) ]
 }
 
-get_s(): 
- Returs 
-    
-    dataframe 
-        nodenames, centralitnames
+# %% --- Crea el tensor final de contribución a centralidad. 
+
+FC_final = [ contrib_to_centrality( ns ) for ns in subsystems.values() ]
+
+FC_final = np.asarray( FC_final ) # A tensor. Dims = (s,c,n)
+FC_final = FC_final.T        # Transposición. Dims = (n,c,s)
+
+outfile1 = open('./tmp/subsystems_centrality','wb'); pickle.dump(FC_final,outfile1); outfile1.close()
+
+# %% --- Función que genera dataframes a pedido
+def fx(subsystem_name): 
+
+    subsystem_keys  = list( subsystems.keys() )
+    subsystem_index = subsystem_keys.index( subsystem_name )
+ 
+    import pandas as pd
+
+    subsystem_frame = FC_final[:,:, subsystem_index ]
+    subsystem_frame = pd.DataFrame( subsystem_frame )
+
+    subsystem_frame.columns = index_centralities
+    subsystem_frame.index   = index_nodes
+
+    return subsystem_frame
