fcf55558b9e991bf9469b8bf3cd6f9f6190337e7
manu-mg <manuel.munoz.g@outlook.com>
2021-03-13 23:10:32 -0300

    Exporta las cosas para el .rmd
    Quedan guardadas en './results' como un monton de sucios CSV porque al parecer no podemos
    serializar desde Python en algo decente para R. Es codigo sucio y feo pero funciona y agh
    me da algo verlo.
    
    To Do:
    - Eliminar esta aberración

--- source/hateful-eight/archivo.py
+++ source/hateful-eight/archivo.py
@@ -1,4 +1,9 @@
 #!/bin/python
+"""
+"An Ixian machine? You defy the Jihad!" 
+"There's a lesson in that, too. What do such machines really do? They increase the number of things we can do without thinking. Things we do without thinking — there's the real danger."
+    - God-Emperor Leto II 
+"""
 
 # %% --- Importar resultados
 import pickle 
@@ -9,45 +14,40 @@ infile = open('./tmp/baseline_centralities' ,'rb'); baseline_centralities  = pic
 infile = open('./tmp/index_nodes','rb'); index_nodes = pickle.load(infile); infile.close()
 infile = open('./tmp/subsystems_dict','rb'); subsystems = pickle.load(infile); infile.close()
 
+# Un error facil de resolver
+index_nodes = list(index_nodes)
+
 # %% --- Importando librerias utiles
 import pandas   as pd
 import numpy    as np
 import networkx as nx
 
-# %% --- Algo informativo sobre el numero de NaNs
+# %% --- Cosas de los subsistemas ???
 
-# count_nan = lambda promedios : np.count_nonzero(np.isnan( promedios ))
-# def nan_info(promedios):
-#     """Devuelve un print con los datos totales, numero de NaNs, y porcentaje"""
-#     print ( pd.Series(
-#         [
-#             promedios.size, 
-#             count_nan( promedios ), 
-#             round(100*count_nan( promedios )/promedios.size, 0)
-#         ], 
-#         index=['Datos totales', 'NaNs', 'Porcentaje NaN'], 
-#         dtype='uint'
-#         ) )
-# 
-# print( 'baseline_centralities' )
-# nan_info( baseline_centralities )
-# print( 'perturbed_centralities' )
-# nan_info( perturbed_centralities )
+# TODO: reemplazar esto por una importación desde Tensor Maker
+def subsystem_tensor( subsystem_nodes, tensor):
+    """Genera un tensor con un subset de nodos basados en el subsistema pedido"""
+    subsystem_index = [ index_nodes.index(node) for node in subsystem_nodes ]
+    return tensor[:, subsystem_index ,:]
 
+# TODO: eliminar esto? 
+# Une dos subsistemas
+subsystem = list( subsystems['glicolisis'] + subsystems['oxphox'])
 
+perturbed_subsystem = subsystem_tensor( subsystem, perturbed_centralities )
+baseline_subsystem  = subsystem_tensor( subsystem, baseline_centralities   )
 
-
-# %% --- Colapsando la tercera dimensión... (centralidades)
+# %% --- Colapsando la segunda dimensión... (nodos totales)
 # Esto calcula 4 promedios de las centralidades para los nodos
 
 from numba import jit
 
 # Un helper porque estas funciones no pueden lidiar con NaNs porque son superiores a los NaN o algo así
-noNaN_perturbed_centralities = np.nan_to_num( perturbed_centralities , copy=True, nan=0.0, posinf=None, neginf=None)
+noNaN_perturbed_subsystem = np.nan_to_num( perturbed_subsystem , copy=True, nan=0.0, posinf=None, neginf=None)
 
 # Es el unico que no require lambdas raros :D
-aritmetic_perturbed = np.nanmean( noNaN_perturbed_centralities, axis= 1)
-aritmetic_baseline  = np.nanmean(        baseline_centralities, axis= 1)
+aritmetic_perturbed = np.nanmean( noNaN_perturbed_subsystem, axis= 1)
+aritmetic_baseline  = np.nanmean(        baseline_subsystem, axis= 1)
 
 # Requiere definir un helper con un factor de correction NoCeros/ValoresTotales
 @jit(nopython=True)
@@ -59,8 +59,8 @@ def geometric_mean(array):
     except: return np.nan
     else : return gmean
 
-geometric_perturbed = np.apply_along_axis( geometric_mean, 1, noNaN_perturbed_centralities)
-geometric_baseline  = np.apply_along_axis( geometric_mean, 1,        baseline_centralities)
+geometric_perturbed = np.apply_along_axis( geometric_mean, 1, noNaN_perturbed_subsystem)
+geometric_baseline  = np.apply_along_axis( geometric_mean, 1,        baseline_subsystem)
 
 # Define una función helper para cuadraticos (Root Square Mean). No en scipy base
 @jit(nopython=True)
@@ -68,8 +68,8 @@ def quadratic_mean(array):
     mean =  np.sqrt( np.nanmean( array*array ) )
     return mean
 
-quadratic_perturbed = np.apply_along_axis( quadratic_mean, 1, noNaN_perturbed_centralities)
-quadratic_baseline  = np.apply_along_axis( quadratic_mean, 1,        baseline_centralities)
+quadratic_perturbed = np.apply_along_axis( quadratic_mean, 1, noNaN_perturbed_subsystem)
+quadratic_baseline  = np.apply_along_axis( quadratic_mean, 1,        baseline_subsystem)
 
 # Requiere definir un helper con un factor de correction NoCeros/ValoresTotales
 @jit(nopython=True)
@@ -81,32 +81,56 @@ def harmonic_mean(array):
     except: return np.nan
     else : return hmean
 
-harmonic_perturbed  = np.apply_along_axis( harmonic_mean,  1, noNaN_perturbed_centralities)
-harmonic_baseline   = np.apply_along_axis( harmonic_mean,  1,        baseline_centralities)
+harmonic_perturbed  = np.apply_along_axis( harmonic_mean,  1, noNaN_perturbed_subsystem)
+harmonic_baseline   = np.apply_along_axis( harmonic_mean,  1,        baseline_subsystem)
 
 # %% --- Divisiones! Calculando como un nodo afecta conectividad de otro
-# 
+# Calcula las metricas para definir los efectos en los nodos
 
-log2_aritmetic = np.log2( aritmetic_baseline / aritmetic_perturbed )
-log2_geometric = np.log2( geometric_baseline / geometric_perturbed )
-log2_quadratic = np.log2( quadratic_baseline / quadratic_perturbed )
-log2_harmonic  = np.log2( harmonic_baseline  / harmonic_perturbed  )
+fold_change_aritmetic = np.log2( aritmetic_baseline / aritmetic_perturbed )
+fold_change_geometric = np.log2( geometric_baseline / geometric_perturbed )
+fold_change_quadratic = np.log2( quadratic_baseline / quadratic_perturbed )
+fold_change_harmonic  = np.log2( harmonic_baseline  / harmonic_perturbed  )
 
-ratio_aritmetic = ( aritmetic_baseline - aritmetic_perturbed ) / aritmetic_baseline
-ratio_geometric = ( geometric_baseline - geometric_perturbed ) / geometric_baseline
-ratio_quadratic = ( quadratic_baseline - quadratic_perturbed ) / quadratic_baseline
-ratio_harmonic  = ( harmonic_baseline  - harmonic_perturbed  ) / harmonic_baseline 
+delta_aritmetic = ( aritmetic_baseline - aritmetic_perturbed ) / aritmetic_baseline
+delta_geometric = ( geometric_baseline - geometric_perturbed ) / geometric_baseline
+delta_quadratic = ( quadratic_baseline - quadratic_perturbed ) / quadratic_baseline
+delta_harmonic  = ( harmonic_baseline  - harmonic_perturbed  ) / harmonic_baseline 
 
-# %% --- 
-# 1056 x 12
-# 
-# delta_centralidad_c = ratio(baseline/pertubed)
-# 
-# nodos_removido x delta_centralidad_c # (1000,12) ~ (1,12)/(1000,12)
-# nodos_removido x delta_centralidad_c
-# 
-# nodos_removido x delta_centralidad_c
-# nodos_removido x delta_centralidad_c
-# 
-# 
-# nodos_removido x delta_centralidad_c_del_sudsystema_agregada
+# %% --- Convierte a Dataframes
+
+index_centralities = ['harmonic_centrality', 'eigenvector_centrality', 'degree_centrality', 'betweenness_centrality', 
+      'closeness_centrality', 'load_centrality', 'information_centrality', 'second_order_centrality',
+      'current_flow_closeness_centrality', 'current_flow_betweenness_centrality',
+       'approximate_current_flow_betweenness_centrality', 'communicability_betweenness_centrality']
+
+def crear_dataframe( matriz ):
+    df = pd.DataFrame(
+        matriz,
+        index = index_nodes,
+        columns = index_centralities
+    )
+    return df
+
+# %% --- Exporta a R
+
+# TODO: eliminar este código estúpido que no deberia existir
+# me da asco verlo y solo existe por la capacidad multi-cursor de VS Code
+#   - Manu, 2021-03-13 23:04
+
+exportar = [ 
+    fold_change_aritmetic , fold_change_geometric , 
+    fold_change_quadratic , fold_change_harmonic , 
+    delta_aritmetic , delta_geometric , 
+    delta_quadratic , delta_harmonic ]
+
+exportar_como = [ 
+    'fold_change_aritmetic' , 'fold_change_geometric' , 
+    'fold_change_quadratic' , 'fold_change_harmonic' , 
+    'delta_aritmetic' , 'delta_geometric' , 
+    'delta_quadratic' , 'delta_harmonic' ]
+
+for i in range(len(exportar)):
+    filename = './results/' + exportar_como[i] + '.csv'
+    frame = crear_dataframe( exportar[i] )
+    frame.to_csv( filename )
