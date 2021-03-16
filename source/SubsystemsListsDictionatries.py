6563c43022682e3e7d8d27b4a85d4bba07836677
Manuel M-G <manuel.munoz.g@outlook.com>
2021-03-16 19:57:16 -0300

    Separa los subsistemas por neurona y astro
    En el renombrado archivo SubsystemsListDictionaries.py, cambia la salida
    para incluir un diccionario con 4 subsistemas, de
    glicolisis y oxphox para neurona y astro, separadamente.

--- source/SubsystemsListsDictionatries.py
+++ source/SubsystemsListsDictionatries.py
@@ -1,15 +1,12 @@
+#!/bin/python
 """
-import random   # DEMOSTRACIÓN DE SUBSISTEMAS RANDOM
-random.seed(23) # DE DISTINTO LAGRGO Y SIMILAR
-
-# EJEMPLO DUMMY DE SUBSISTEMAS
-subsystems = {
-    'ns1' : [ index_nodes[n] for n in random.sample(range(0, 1055), 7)  ] ,
-    'ns2' : [ index_nodes[n] for n in random.sample(range(0, 1055), 11) ]
-}"""
-
+"Of the Horcrux, wickedest of magical inventions, we shall not speak nor give direction"
+    — Magick Moste Evile
+"""
+# %% --- IMPORTA EL MODELO Y COSAS
 import cobra 
 
+# TODO: que use una variable ambiental? Aunque igual es manualmente curado
 INPUT_MODEL = './data/stimulated_2021.json'
 
 import warnings; warnings.filterwarnings('ignore') # Ignora warnings
@@ -31,14 +28,19 @@ glicolisis = tabla_primaria[tabla_primaria["subsystem"].str.contains('Glycolysis
 # Esta algo contaminado con reacciones que no corresponden a glicolisis, por lo que hare una curación manual para seleccionar
 # solo las relevantes a glicolisis y gluconeogenesis
 
-glicolisis_ids = ['ENO', 'FBA', 'GAPD', 'HEX1', 'PFK', 'PGI', 'PGK', 'PGM', 'PYK', 'TPI'] # Curado manual
+glicolisis_ids_astros = ['ENO', 'FBA', 'GAPD', 'HEX1', 'PFK', 'PGI', 'PGK', 'PGM', 'PYK', 'TPI'] # Curado manual
 # PEPCK no existe en el grafo porque no es fully-conected
-glicolisis_ids = glicolisis_ids + [ ids + '_Neuron' for ids in glicolisis_ids ] # Stimulated tiene "IDs duplicados" por ID_Neuron
+glicolisis_ids_neuron = [ ids + '_Neuron' for ids in glicolisis_ids_astros ] # Stimulated tiene "IDs duplicados" por ID_Neuron
 
-glicolisis = tabla_primaria[tabla_primaria['ids'].isin( glicolisis_ids )] # Dataframe con reacciones de glicolisis curadas
-glicolisis.to_csv( './tmp/glicolisis.csv' , index=False ) # Salida a CSV para legilibilidad y eso
+glicolisis_astros = tabla_primaria[tabla_primaria['ids'].isin( glicolisis_ids_astros )] # Dataframe con reacciones de glicolisis curadas
+glicolisis_astros.to_csv( './tmp/glicolisis_astros.csv' , index=False ) # Salida a CSV para legilibilidad y eso
 
-glicolisis_ids = list( glicolisis['ids'] ) # Es necesario que queden absolutamente iguales
+glicolisis_neuron = tabla_primaria[tabla_primaria['ids'].isin( glicolisis_ids_neuron )] # Dataframe con reacciones de glicolisis curadas
+glicolisis_neuron.to_csv( './tmp/glicolisis_neuron.csv' , index=False ) # Salida a CSV para legilibilidad y eso
+
+# SANITIY CHECK DE IDS CORRESPONDIENDO AL MODELO
+glicolisis_ids_astros  = list( glicolisis_astros['ids'] ) # Es necesario que queden absolutamente iguales
+glicolisis_ids_neuron = list( glicolisis_neuron['ids'] ) # same 
 
 # %% --- REACCIONES EXPLORATORIAS PARA GLICOLISIS
 
@@ -49,12 +51,17 @@ oxphox.to_csv( './tmp/oxphox.csv' , index=False ) # Salida a CSV para legilibili
 
 oxphox_ids = list( oxphox['ids'] ) # Es necesario que queden absolutamente iguales
 
+oxphox_ids_astros = [ id for id in oxphox_ids if not id.endswith('_Neuron')]
+oxphox_ids_neuron = [ id for id in oxphox_ids if id.endswith('_Neuron')]
+
 # %% --- DICCIONARIO DE SUBSISTEMAS MANUALMENTE CURADOS
 # Este es para pasarlo a otros analisis, como un objeto Python
 
 subsystems_dict = {
-    'glicolisis' : glicolisis_ids, 
-    'oxphox'     : oxphox_ids
+    'glicolisis_astros' : glicolisis_ids_astros, 
+    'oxphox_astros'     : oxphox_ids,
+    'glicolisis_neuron' : glicolisis_ids_neuron, 
+    'oxphox_neuron'     : oxphox_ids
 }
 
 import pickle 
