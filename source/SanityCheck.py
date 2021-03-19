1383b6dcf98b76b905d9cd230ebb2a72476059c2
Manuel M-G <manuel.munoz.g@outlook.com>
2021-03-19 21:05:14 +0000

    Sanity Check
    Porque al parecer los resultados del cluster eran distintos a las de
    todas las cosas
    
    Co-authored-by: Alejandro Acevedo, PhD. <alejandroacevedo2305@users.noreply.github.com>

---
+++ source/SanityCheck.py
@@ -0,0 +1,13 @@
+"""
+"""
+
+import numpy  as np
+import pandas as pd
+
+local_baseline   = pd.read_csv('glico_resultado_local.csv', index_col=0)
+cluster_baseline = pd.read_csv('glic2.csv', index_col=0)
+
+BASELINES = np.allclose(local_baseline.to_numpy(), cluster_baseline.to_numpy(), 
+    rtol=1e-05, atol=1e-08, equal_nan=False)
+
+print('Baselines son iguales?:', BASELINES)
