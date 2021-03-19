ac986d082d1f73fa43317cf77db510b332939c78
Manuel M-G <manuel.munoz.g@outlook.com>
2021-03-19 21:25:07 +0000

    Annade los nodos removidos al SanityCheck.py

--- source/SanityCheck.py
+++ source/SanityCheck.py
@@ -11,3 +11,11 @@ BASELINES = np.allclose(local_baseline.to_numpy(), cluster_baseline.to_numpy(),
     rtol=1e-05, atol=1e-08, equal_nan=False)
 
 print('Baselines son iguales?:', BASELINES)
+
+local_removed   = pd.read_csv('perturbed_naive_FPGS3m_short.csv', index_col=0)
+cluster_removed = pd.read_csv('removed_FPGS3m.csv.csv', index_col=0)
+
+REMOVED = np.allclose(local_baseline.to_numpy(), cluster_baseline.to_numpy(), 
+    rtol=1e-05, atol=1e-08, equal_nan=False)
+
+print('Removidos son iguales?:', REMOVED)
