de4c4cb7dc2012aa5d9a8270ecfd2ddf985c6342
Alejandro Acevedo A <a.acevedo.aracena@gmail.com>
2021-03-24 17:47:51 -0300

    Modifica las centralidades usadas para el calculo
    Remueve varias, y adiciona katz y pagerank.
    
    To Do:
    - Tirar esto al cluster

--- source/centrality_baseline_and_node_removal.py
+++ source/centrality_baseline_and_node_removal.py
@@ -77,7 +77,6 @@ def compute_centralities_short(graph):
     centralities = {
         'harmonic_centrality' : hc ,
         'eigenvector_centrality' : ec ,
-        'degree_centrality' : dc ,
         'closeness_centrality' : cc ,
         'information_centrality' : ic
     }
@@ -93,32 +92,26 @@ def compute_centralities(graph, lite=LITE):
         # TODO: supuestamente estos se pueden poner internamente como 'float32', que es suficiente y consume menos memoria
         hc    = nx.harmonic_centrality(graph, nbunch=None, distance=None)
         ec    = nx.eigenvector_centrality(graph, max_iter=1000, tol=1e-05, nstart=None, weight=None)
-        dc    = nx.degree_centrality(graph)
         bc    = nx.betweenness_centrality(graph, normalized=True, weight=None, endpoints=False, seed=None)
         cc    = nx.closeness_centrality(graph, distance=None, wf_improved=True)
         lc    = nx.load_centrality(graph, cutoff=None, normalized=True, weight=None)
         ic    = nx.information_centrality(graph) # Requiere scipy
-        soc   = nx.second_order_centrality(graph) 
-        cfcc  = nx.current_flow_closeness_centrality(graph) # Requiere scipy
-        cfbc  = nx.current_flow_betweenness_centrality(graph)
-        acfbc = nx.approximate_current_flow_betweenness_centrality(graph)
         cbc   = nx.communicability_betweenness_centrality(graph) 
+        kz    = nx.katz_centrality_numpy(graph)
+        pr    = nx.pagerank(graph)
 
         #centralities = [hc, ec, dc, bc, cc, lc, ic, soc, cfcc, cfbc, acfbc, cbc]
         # CREA UN DICCIONARIO DE DICCIONARIOS PARA PASARLE EL OBJETO A PANDAS
         centralities = {
             'harmonic_centrality' : hc ,
             'eigenvector_centrality' : ec ,
-            'degree_centrality' : dc ,
             'betweenness_centrality' : bc ,
             'closeness_centrality' : cc ,
             'load_centrality' : lc ,
             'information_centrality' : ic ,
-            'second_order_centrality' : soc ,
-            'current_flow_closeness_centrality' : cfcc ,
-            'current_flow_betweenness_centrality' : cfbc ,
-            'approximate_current_flow_betweenness_centrality' : acfbc ,
-            'communicability_betweenness_centrality' : cbc 
+            'communicability_betweenness_centrality' : cbc ,
+            'katz_centrality_numpy' : kz ,
+            'pagerank' : pr
         }
         
         # CONVIERTE LAS CENTRALIDADES A UN DATAFRAME DE PANDAS
