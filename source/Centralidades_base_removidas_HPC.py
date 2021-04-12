20d552185fe9d50338230475535141dbf1b70ea4
Manuel M-G <manuel.munoz.g@outlook.com>
2021-04-12 18:06:05 -0400

    Reescribe centralidades removidas
    Modificación casi total. Ya no genera el baseline.pkl, y la distribucion
    es en sets de 5 workers por nodo, por lo nefasto de Recon
    
    To Do:
    - Testear que tanto esto rompe el procesamiento

--- source/Centralidades_base_removidas_HPC.py
+++ source/Centralidades_base_removidas_HPC.py
@@ -5,194 +5,206 @@
 
 Este es codigo para ejecutar en el cluster virtual Ray dentro del HPC. No intentes ejecutar esto en casa. 
 """
-# %% --- IMPORTA EL GRAFO GENERADO
-
+# %%% --- IMPORTING BASE LIBRARIES
 import networkx as nx
 import numpy    as np
-import pandas   as pd  
+import pandas   as pd
+
+import os
+import time 
 
 import pickle
 
-LITE=False # Variable para definir si los calculos deben ser complejos o no
+# %% --- USING THE RAY LIBRARY FOR DISTRIBUITED COMPUTING
+import ray
+
+#ray.init(address='auto', _redis_password='5241590000000000') # Use this for local
+ray.init(address='auto', _node_ip_address=os.environ["ip_head"].split(":")[0], _redis_password=os.environ["redis_password"])
+
+# %% --- WARPER FUNCTIONS FOR RAY
+
+@ray.remote
+def remove_node(graph, node):
+    """Generates a new graph whitout the selected node
+
+    Args:
+        graph (graph): the base graph
+        node (str): the node to remove
+    
+    Returns:
+        node (str): the node to remove as sanity check
+        graph_node_removed (graph): the largest component of the graph without the node
+    """
+
+    assert node in list( graph.nodes ), "The selected node is not part of the graph"
+
+    G_removed = graph.copy() # CREA UNA COPIA DE TRABAJO PARA EL GRAFO
+    G_removed.remove_node( node )  # ELIMINA EL NODO A ITERAR
+
+    # Selects the largest component
+    largest_component = max(nx.connected_components(G_removed), key=len)
+
+    graph_node_removed = G_removed.subgraph(largest_component) # Subsets the largest component
+
+    assert nx.is_connected(graph_node_removed), "The graph is not fully conected"
+
+    return node, graph_node_removed
+
+# As these don't take forever (except closeness centrality), is more efficient
+# to group them in a single process for the distribuited computing step. 
+@ray.remote
+def fast_centralities(graph, node, alpha=0.0001):
+    """Eigenvector, Pagerank, Katz, Global reaching, and closeness centrality
+    
+    [0] Pass and return removed node as sanity check  
+    [1] Dataframe whith results  
+    [2] Compute time  
+    """
+
+    tic = time.time()
+
+    ec  = nx.eigenvector_centrality(graph, max_iter=1000, tol=1e-05, nstart=None, weight=None)
+    pr  = nx.pagerank(graph, alpha = alpha)
+    kz  = nx.katz_centrality(graph, alpha = alpha)
+    grc = nx.global_reaching_centrality(graph)
+    cc  = nx.closeness_centrality(graph, distance=None, wf_improved=True) # Takes forever
+
+    fast_centralities_df = pd.DataFrame({
+        "eigenvector_centrality" : ec ,
+        "pagerank" : pr ,
+        "katz_centrality" : kz ,
+        "global_reaching_centrality" : grc ,
+        "closeness_centrality" : cc
+    })
+
+    toc = time.time()
+
+    return node, fast_centralities_df , (toc - tic)
+
+# And these take forever, so we distribuite these in individual workers
+
+@ray.remote
+def harmonic_centrality(graph, node):
+    tic = time.time()
+    out = nx.harmonic_centrality(graph, nbunch=None, distance=None)
+    df_harmonic_centrality = pd.DataFrame({
+        "harmonic_centrality" : out
+    })
+    toc = time.time()
+    return out , df_harmonic_centrality,  (toc - tic)
+
+@ray.remote
+def information_centrality(graph, node):
+    tic = time.time()
+    out = nx.information_centrality(graph)
+    df_information_centrality = pd.DataFrame({
+        "information_centrality" : out
+    })
+    toc = time.time()
+    return out , df_information_centrality, (toc - tic)
+
+@ray.remote
+def load_centrality(graph, node):
+    tic = time.time()
+    out = nx.load_centrality(graph, cutoff=None, normalized=True, weight=None)
+    df_load_centrality = pd.DataFrame({
+        "load_centrality" : out
+    })
+    toc = time.time()
+    return out , df_load_centrality , (toc - tic)
+
+@ray.remote
+def betweenness_centrality(graph, node):
+    tic = time.time()
+    out = nx.betweenness_centrality(graph, normalized=True, weight=None, endpoints=False, seed=None)
+    print( time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+            '--- Resolved', node) # This is the slowest, so every other should be done 
+    df_betweenness_centrality = pd.DataFrame({
+        "betweenness_centrality" : out
+    })
+    toc = time.time()
+    return out , df_betweenness_centrality , (toc - tic)
+
+# %% --- IMPORTING BASE GRAPH
 
 G = nx.read_gpickle('./data/Recon2_rxn_proyected.gpickle')
 
-# %% --- DEFINICIONES DE FUNCIONES
+G_ray = ray.put(G) # BASE GRAPH TO OBJECT STORE
 
-def get_largest_component(grafo): 
-    import networkx as nx
-    largest_component = max(nx.connected_components(grafo), key=len)
-    G = grafo.subgraph(largest_component)
-    return G
+# %% --- CREATING THE GRAPHS WITH REMOVED NODES
 
-def compute_centralities_short(graph):
-    """Computa las centralidades rapidas y devuelve una DataFrame (no reindexado) con estas"""
-    # TODO: supuestamente estos se pueden poner internamente como 'float32', que es suficiente y consume menos memoria
-    hc    = nx.harmonic_centrality(graph, nbunch=None, distance=None)
-    ec    = nx.eigenvector_centrality(graph, max_iter=1000, tol=1e-05, nstart=None, weight=None)
-    # dc    = nx.degree_centrality(graph)
-    cc    = nx.closeness_centrality(graph, distance=None, wf_improved=True)
-    ic    = nx.information_centrality(graph) # Requiere scipy
+infile = open('./tmp/subsystems_dict.pkl', 'rb'); subsystems_dict = pickle.load(infile); infile.close()
 
-    # CREA UN DICCIONARIO DE DICCIONARIOS PARA PASARLE EL OBJETO A PANDAS
-    centralities = {
-        'harmonic_centrality' : hc ,
-        'eigenvector_centrality' : ec ,
-        'closeness_centrality' : cc ,
-        'information_centrality' : ic
-    }
+# List of nodes to remove
+NODES_REMOVED = subsystems_dict['Phenylalanine metabolism'] + ['r0399']
 
-    # CONVIERTE LAS CENTRALIDADES A UN DATAFRAME DE PANDAS
-    centralities = pd.DataFrame( centralities )
+# This launches Ray actors to generate graphs whitout one node from the list
+graph_removed = [ remove_node.remote( G_ray , node ) for node in NODES_REMOVED ]
+graph_removed = ray.get( graph_removed )
 
-    return centralities
+# This sets the returned list of lists in a dictionay { node : graph_node_removed }
+graph_removed = { rm[0] : rm[1] for rm in graph_removed }
 
-def compute_centralities(graph, lite=False, alpha=0.0003):
-    """Computa las doce centralidades y devuelve una DataFrame (no reindexado) con estas"""
-    if lite == False:
-        # TODO: supuestamente estos se pueden poner internamente como 'float32', que es suficiente y consume menos memoria
-        dc    = nx.degree_centrality(graph) # SANITY CHECK: Deberia ser similar a todo lo demas
-        hc    = nx.harmonic_centrality(graph, nbunch=None, distance=None)
-        ec    = nx.eigenvector_centrality(graph, max_iter=1000, tol=1e-05, nstart=None, weight=None)
-        bc    = nx.betweenness_centrality(graph, normalized=True, weight=None, endpoints=False, seed=None)
-        cc    = nx.closeness_centrality(graph, distance=None, wf_improved=True)
-        lc    = nx.load_centrality(graph, cutoff=None, normalized=True, weight=None)
-        ic    = nx.information_centrality(graph) # Requiere scipy
-        cbc   = nx.communicability_betweenness_centrality(graph) 
-        kz    = nx.katz_centrality(graph, alpha = alpha)
-        pr    = nx.pagerank(graph, alpha = alpha)
+# TODO: this may be a bad idea, as removed_nodes x graph = lots of space in Object Store
+graph_rm_ray = ray.put(graph_removed) # Sends this to the object store
 
-        #centralities = [hc, ec, dc, bc, cc, lc, ic, soc, cfcc, cfbc, acfbc, cbc]
-        # CREA UN DICCIONARIO DE DICCIONARIOS PARA PASARLE EL OBJETO A PANDAS
-        centralities = {
-            'degree_centrality' : dc ,
-            'harmonic_centrality' : hc ,
-            'eigenvector_centrality' : ec ,
-            'betweenness_centrality' : bc ,
-            'closeness_centrality' : cc ,
-            'load_centrality' : lc ,
-            'information_centrality' : ic ,
-            'communicability_betweenness_centrality' : cbc ,
-            'katz_centrality_numpy' : kz ,
-            'pagerank' : pr
-        }
-        
-        # CONVIERTE LAS CENTRALIDADES A UN DATAFRAME DE PANDAS
-        centralities = pd.DataFrame( centralities )
-    
-    else: 
-        # COMPUTA LA VERSION LOW-COST DE LAS CENTRALIDADES  
-        centralities = compute_centralities_short(graph)
+print( time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), '--- Done node removal' )
 
-    return centralities
+# %% --- LAUNCHING THE CENTRALITY CALCULATIONS
+# This launches to the distribuited process in the Ray cluster
+fast = [ fast_centralities.remote( graph_rm_ray[node] , node )      for node in graph_rm_ray] 
+harc = [ harmonic_centrality.remote( graph_rm_ray[node] , node )    for node in graph_rm_ray] 
+info = [ information_centrality.remote( graph_rm_ray[node] , node ) for node in graph_rm_ray] 
+load = [ load_centrality.remote( graph_rm_ray[node] , node )        for node in graph_rm_ray] 
+btwn = [ betweenness_centrality.remote( graph_rm_ray[node] , node ) for node in graph_rm_ray] 
 
-def get_alpha(G):
-    """"Calcula el parametro alpha para PageRank y Katz"""
-    tmp = nx.adjacency_matrix(G).toarray()
+# %% --- RETURNING THE CENTRALITIES
+# Lists of [[node, dataframe, time], ...] for NODES_REMOVED
+fast = ray.get( fast ) 
+harc = ray.get( harc ) 
+info = ray.get( info ) 
+load = ray.get( load ) 
+btwn = ray.get( btwn ) 
 
-    tmp = np.linalg.eigvals(tmp)
-    tmp = np.max(tmp)
-    tmp = np.real(tmp)    
+# %% --- CENTRALITIES TO DATAFRAMES
+# Dictionaries to track node and dataframe
+fast_df = { node[0] : node[1] for node in fast }
+harc_df = { node[0] : node[1] for node in harc }
+info_df = { node[0] : node[1] for node in info }
+load_df = { node[0] : node[1] for node in load }
+btwn_df = { node[0] : node[1] for node in btwn }
 
-    tmp = .5*(1/tmp)
+# Joining dataframes onto single dataframe for each removed node
+def join_centrality_dataframes( node ):
+    tmp = fast_df[node] # Loads the first dataframe
+    tmp = tmp.join( harc_df[node] , how="inner") # joins the harmonic_centrality
+    tmp = tmp.join( info_df[node] , how="inner") # joins the information_centrality
+    tmp = tmp.join( load_df[node] , how="inner") # joins the load_centrality
+    tmp = tmp.join( btwn_df[node] , how="inner") # joins the betweenness_centrality
 
     return tmp
 
-# %% --- CALCULO DE CENTRALIDADES CON NODOS REMOVIDOS
+centralities_final_dict = { node : join_centrality_dataframes( node ) for node in NODES_REMOVED } 
 
-import time
+outfile = open('./tmp/centralidades_perturbadas.pkl', 'wb'); pickle.dump( centralities_final_dict , outfile); outfile.close()
 
-def removed_nodes_centrality(graph, node, info=False):
-    """Helper que remueve un nodo y calcula la centralidad de este"""
-    print( time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
-        '--- Iterando en nodo', node, # Sanity check de nodo iterado
-        '--- Memoria en uso:', ( ray.available_resources()['memory'] - ray.cluster_resources()['memory'] )) # Uso de memoria
-    
-    G_removed = graph.copy() # CREA UNA COPIA DE TRABAJO PARA EL GRAFO
+print( time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), '--- Done centrality for removed nodes' )
 
-    G_removed.remove_node( node )  # ELIMINA EL NODO A ITERAR
-    G_removed = get_largest_component(G_removed) # ELIMINA NODOS DISCONEXOS
+# %% --- TIMING OF THE CALCULATIONS
+fast_time = { node[0] : node[2] for node in fast }
+harc_time = { node[0] : node[2] for node in harc }
+info_time = { node[0] : node[2] for node in info }
+load_time = { node[0] : node[2] for node in load }
+btwn_time = { node[0] : node[2] for node in btwn }
 
-    assert len(graph.nodes) != len(G_removed.nodes), "Ningun nodo removido."
-    nodos_removidos = set(graph.nodes) - set(G_removed.nodes)
+df_times = pd.DataFrame({
+    "fast_and_closeness_centralities" : fast_time ,
+    "harmonic_centrality" : harc_time ,
+    "information_centrality" : info_time ,
+    "load_centrality" : load_time ,
+    "betweenness_centrality" : btwn_time
+})
 
-    if info:
-        # INFORMACION EXTRA PARA LOS CALCULOS Y DEMAS
-        print( 'Nodos originales:', len(graph.nodes) )
-        print( 'Nodos post-remocion:', len(G_removed.nodes) )
-        print( 'Nodos removidos:', len(graph.nodes) - len(G_removed.nodes) )
-        print( 'Removidos:', nodos_removidos )
+df_times.to_csv("tmp.time_removed_nodes.csv")
 
-    removed_centrality = compute_centralities(G_removed, lite=False, alpha = 0.001) # CENTRALIDADES
-
-    all_nodes = list( graph.nodes ) # REINDEXANDO PARA INCLUIR REMOVIDO
-    removed_centrality = removed_centrality.reindex( all_nodes )
-
-    print( 'SANITY_CHECK:', node, 'DIMENSIONES_DATAFRAME:', removed_centrality.shape )
-
-    removed_centrality.name = str( node ) # RENOMBRADO DEL DATAFRAME
-
-    # SANITY CHECK PARA VER QUE EL DATAFRAME TENGA COMO NAN LOS REMOVIDOS
-    for removido in nodos_removidos:
-        assert np.isnan( removed_centrality.loc[ removido , 'degree_centrality']), 'Nodo removido tiene un valor no NaN. Error de DataFrame.'
-
-    return node, removed_centrality
-
-# %% --- CALCULO DE CENTRALIDADES CON (ITERATIVOS) NODOS REMOVIDOS
-
-import ray
-
-@ray.remote
-def hpc_reemoved_centrality( graph, nodes_to_remove ):
-    """Calcula las centralidades para nodos removidos de forma distribuida en un cluster"""
-
-    assert type( nodes_to_remove ) == list, 'El input de remocion no es una lista. Usa list()'
-
-    centralities_removed = [ removed_nodes_centrality(graph, node) for node in nodes_to_remove ]
-
-    return centralities_removed
-
-# %% --- COMPUTA LAS BASELINE DEL SISTEMA
-
-if __name__ == '__main__':
-
-    # INICIALIZA LA CONEXION AL SERVIDOR DE RAY
-    import os # Importa variables ambientales
-    ray.init(address='auto', _node_ip_address=os.environ["ip_head"].split(":")[0], _redis_password=os.environ["redis_password"])
-    #ray.init(address='auto', _redis_password='5241590000000000')
-
-    G_ray = ray.put( G ) # Sube el grafo al object store
-
-    # Importa la lista de subsistemas de un archivo externo
-    infile = open('./tmp/subsystems_dict.pkl', 'rb'); subsystems_dict = pickle.load(infile); infile.close()
-
-    nodos_remover = subsystems_dict['Phenylalanine metabolism'] + ['r0399']
-
-    # EVITAR EL SOBRE-PARALELISMO DE UN PROCESO POR NODO
-    WORKERS = int( ray.cluster_resources()['CPU'] ) # CANTIDAD DE CPUS DEL CLUSTER
-    SPLITS = min(WORKERS, len(nodos_remover) ) # Numero de CPUs en el cluster, o nodos en la red
-    
-    print('Distribuyendo', len(nodos_remover), 'nodos en', SPLITS, 'procesos.') # Sanity check
-    
-    nodos_remover = np.array_split( nodos_remover , SPLITS )  # Lo separa en listas mas pequenas
-    nodos_remover = [ list(chunk) for chunk in nodos_remover ] # Convierte a lista de nuevo
-
-    # CALCULO DE CENTRALIDADES PERTURBADAS (MANDA LA TAREA AL CLUSTER VIRTUAL)
-    centralidades_distribuidas = [ hpc_reemoved_centrality.remote( G_ray, chunk ) for chunk in nodos_remover ]
-
-    # INTERMEDIO EN QUE LOCALMENTE CALCULA LAS CENTRALIDADES BASE. MENOS DEMANDANTE QUE EL RESTO
-    baseline = compute_centralities(G, lite=LITE, alpha=0.0003) # TIRA EL CALCULO DE CENTRALIDADES BASE
-    print( baseline.info(verbose=True) ) # Sanity check para el formato de las salidas
-
-    # VENGANZA DE LOS PROCESOS ENVIADOS AL CLUSTER (GET.REMOTES)
-    centralidades_perturbadas = ray.get( centralidades_distribuidas )
-    centralidades_perturbadas = [ii for i in centralidades_perturbadas for ii in i] # Aplasta la lista
-
-    centralidades_perturbadas = { cent[0] : cent[1] for cent in centralidades_perturbadas }
-
-    # GUARDANDO RESULTADOS
-    baseline = {'baseline' : baseline } # Para que queden igual
-    
-    import pickle
-    outfile = open('./tmp/baseline.pkl', 'wb'); pickle.dump( baseline ,outfile); outfile.close()
-    outfile = open('./tmp/centralidades_perturbadas.pkl', 'wb'); pickle.dump( centralidades_perturbadas ,outfile); outfile.close()
+print( time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), '--- Done times for centralities' )
