1e5bf8e1c599c454ad3f91a24e3eb08c0af704da
Alejandro Acevedo A <a.acevedo.aracena@gmail.com>
2021-06-18 15:03:15 -0400

    samples

---
+++ source/matlab/Leftraru_shrinking_Recon2_ACHR.m
@@ -0,0 +1,32 @@
+addpath(genpath('/home/aacevedo'));
+initCobraToolbox;
+changeCobraSolver('gurobi');
+myPath = '/home/aacevedo/NLHPC/models';
+recon = readCbModel([myPath filesep 'small_metabolism.mat']);
+model = recon;
+sol = optimizeCbModel(model);sol.f
+model.lb(sol.x<0)  = sol.x(sol.x<0);
+model.ub(sol.x<0)  = 0;
+model.lb(sol.x>0)  = 0;
+model.ub(sol.x>0)  = sol.x(sol.x>0);
+model.lb(sol.x==0) = 0;
+model.ub(sol.x==0) = 0;
+sol2 = optimizeCbModel(model);sol2.f
+sampleFile                    =   [];
+samplerName                   =  'ACHR';
+options.nStepsPerPoint        =  10;%- Number of sampler steps per point saved (200)
+options.nPointsReturned       =  500;%- Number of points loaded for analysis (2000)
+options.nWarmupPoints         =  10;%. - Number of warmup points (5000). ACHR only.
+options.nFiles                =  50;%. - Number of output files (10). ACHR only.
+options.nPointsPerFile        =  500;%. - Number of points per file (1000). ACHR only.
+%options.nFilesSkipped        =  %. - Number of output files skipped when loading points to avoid potentially biased initial samples (2) loops (true). ACHR only.
+options.maxTime               =  36000; %. - Maximum time limit (Default = 36000 s). ACHR only.
+%options.toRound              =  %. - Option to round the model before sampling (true). CHRR only.
+%options.lambda               =  %. - the bias vector for exponential sampling. CHRR_EXP only.
+modelSampling                 =  [];
+[modelSampled,samples_leftraru ,volume_leftraru ] = sampleCbModel(model, sampleFile, samplerName, options, modelSampling);
+ACHR_shrunk_leftraru          = modelSampled;
+
+%clear  myPath modelSampled model sampleFile samplerName options modelSampling
+scriptPath = '/home/aacevedo/NLHPC/output';
+csvwrite([scriptPath filesep 'samples_leftraru.csv'], samples_leftraru) 
---
+++ source/matlab/shinking_Recon2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name= samplig_Recon3D_ACHR
+#SBATCH --mail-user=deepen.data@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=%j_%x.out
+#SBATCH --error=%j_%x.err
+#SBATCH --partition=slims
+#SBATCH -N 1
+#SBATCH -n 10
+#SBATCH -L matlab,matlab-distrib_computing
+#SBATCH --mem=48000
+
+module load Matlab/2017
+module load gurobi/7.5.1
+
+mkdir -p /tmp/${SLURM_JOB_ID}/{lcj,mcr}
+export MCR_CACHE_ROOT=/tmp/${SLURM_JOB_ID}/mcr
+
+export MATLAB_N=$1
+
+matlab -nodisplay -nosplash -nodesktop < /home/aacevedo/NLHPC/Scripts/Leftraru_shrinking_Recon2_ACHR.m
